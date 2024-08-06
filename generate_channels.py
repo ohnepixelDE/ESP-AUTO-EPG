c9e59b1928f8193f8b5f15c4fbbf3466be374209
boogabooga841 <kebabremover1234@outlook.com>
2024-08-06 14:51:57 +0300

    only US UK RU UA channels

---
+++ generate_channels.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as ET
+import os
+
+def filter_channels(input_file, output_file):
+    tree = ET.parse(input_file)
+    root = tree.getroot()
+    
+    filtered_root = ET.Element(root.tag)
+    
+    for channel in root.findall('channel'):
+        xmltv_id = channel.get('xmltv_id')
+        
+        if xmltv_id and (xmltv_id.endswith('.us') or xmltv_id.endswith('.uk') or xmltv_id.endswith('.ru') or xmltv_id.endswith('.ua')):
+            filtered_root.append(channel)
+    
+    filtered_tree = ET.ElementTree(filtered_root)
+    filtered_tree.write(output_file, encoding='utf-8', xml_declaration=True)
+
+input_file = 'sites/tvpassport.com/tvpassport.com.channels.xml'
+output_file = 'updated_channels.xml'
+
+filter_channels(input_file, output_file)
